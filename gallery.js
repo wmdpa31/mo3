7c9f5e75514d7b904c1b95ea90fd5f1031ecbb66
소우주 <sowoojoo@soujuui-MacBookPro.local>
2024-07-01 15:38:31 +0900

    url 변경

--- gallery.js
+++ gallery.js
@@ -52,16 +52,16 @@ window.onload = () => {
         imageWidth: 1200,
         imageHeight: 630,
         link: {
-          mobileWebUrl: "https://mo2.luvle.co.kr",
-          webUrl: "https://mo2.luvle.co.kr",
+          mobileWebUrl: "https://mo3.luvle.co.kr",
+          webUrl: "https://mo3.luvle.co.kr",
         },
       },
       buttons: [
         {
           title: "자세히 보기",
           link: {
-            mobileWebUrl: "https://mo2.luvle.co.kr",
-            webUrl: "https://mo2.luvle.co.kr",
+            mobileWebUrl: "https://mo3.luvle.co.kr",
+            webUrl: "https://mo3.luvle.co.kr",
           },
         },
       ],
